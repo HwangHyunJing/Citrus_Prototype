4bea7efdde2f9b5f0607c71b2bde79e21b5fe672
HwangHyunJing <bbq2277@naver.com>
2023-12-22 20:18:41 +0900

    231222_1 [SOLVED] (1) Character now poses Run, Crawl, Crouch, In Air, and Land. (2) Manage motions with ABP_ (FSM) (3) Display Widget BP through Game Mode Base code.
    [MODIFIED]
    (1) Add some scripts for game mode widget.
    (2) Add some animation assets
    
    [DELAYED]
    (1) Exclaim, Jump and Air Jump motion delayed to later.

--- Source/LemonSour/LemonSourGameModeBase.cpp
+++ Source/LemonSour/LemonSourGameModeBase.cpp
@@ -3,3 +3,30 @@
 
 #include "LemonSourGameModeBase.h"
 
+// À§Á¬ ÄÄÆ÷³ÍÆ®
+#include "Components/WidgetComponent.h"
+
+ALemonSourGameModeBase::ALemonSourGameModeBase()
+{
+	/*WidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComponent"));
+	WidgetComponent->SetupAttachment(GetRootComponent());*/
+
+
+}
+
+void ALemonSourGameModeBase::BeginPlay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("begin play called!"));
+
+	if (IsValid(WidgetClass))
+	{
+		GameModeWidget = Cast<UGameModeWidget>(CreateWidget(GetWorld(), WidgetClass));
+		UE_LOG(LogTemp, Warning, TEXT("casted!"));
+
+		if (GameModeWidget != nullptr)
+		{
+			GameModeWidget->AddToViewport();
+			UE_LOG(LogTemp, Warning, TEXT("added!"));
+		}
+	}
+}
--- Source/LemonSour/LemonSourGameModeBase.h
+++ Source/LemonSour/LemonSourGameModeBase.h
@@ -1,17 +1,31 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "HUD/GameModeWidget.h"
 #include "LemonSourGameModeBase.generated.h"
 
-/**
- * 
- */
+class UWidgetComponent;
+class UGameModeWidget;
+
 UCLASS()
 class LEMONSOUR_API ALemonSourGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	// 생성자
+	ALemonSourGameModeBase();
+
+	virtual void BeginPlay() override;
+
+	// gamemode가 사용할 위젯: 일단 BP에서 넘겨보기
+	/*UPROPERTY(VisibleAnywhere)
+	UWidgetComponent* WidgetComponent;*/
+
+	UPROPERTY(EditAnywhere, Category = "ClassTypes")
+	TSubclassOf<UUserWidget> WidgetClass;
+
+	UPROPERTY(VisibleInstanceOnly, Category = "Runtime")
+	UGameModeWidget* GameModeWidget;
 };
