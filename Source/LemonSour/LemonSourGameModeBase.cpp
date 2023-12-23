982bfa5dac8d9e643a9329cd4361ab186c84213a
HwangHyunJing <bbq2277@naver.com>
2023-12-24 01:11:51 +0900

    231223_2 [SOLVED] (1) Can restrict Y, Z (by instance detail) (2) Display string through widget component

--- Source/LemonSour/LemonSourGameModeBase.cpp
+++ Source/LemonSour/LemonSourGameModeBase.cpp
@@ -8,25 +8,22 @@
 
 ALemonSourGameModeBase::ALemonSourGameModeBase()
 {
-	/*WidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComponent"));
-	WidgetComponent->SetupAttachment(GetRootComponent());*/
-
 
 }
 
 void ALemonSourGameModeBase::BeginPlay()
 {
-	UE_LOG(LogTemp, Warning, TEXT("begin play called!"));
+	// UE_LOG(LogTemp, Warning, TEXT("begin play called!"));
 
 	if (IsValid(WidgetClass))
 	{
 		GameModeWidget = Cast<UGameModeWidget>(CreateWidget(GetWorld(), WidgetClass));
-		UE_LOG(LogTemp, Warning, TEXT("casted!"));
+		// UE_LOG(LogTemp, Warning, TEXT("casted!"));
 
 		if (GameModeWidget != nullptr)
 		{
 			GameModeWidget->AddToViewport();
-			UE_LOG(LogTemp, Warning, TEXT("added!"));
+			// UE_LOG(LogTemp, Warning, TEXT("added!"));
 		}
 	}
 }
